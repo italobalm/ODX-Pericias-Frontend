527566f313e1782eed41ec4a711e35c0aea675b5
Italo Melo <balmprojects@gmail.com>
2025-03-20 10:32:19 -0300

    feature: criando tela de cadastro e melhorando o estilo da página de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,20 +2,31 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa"; // importando um ícone de seta
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault(); //Adicionando preventDefault pra quando o user clicar em entrar, não recarregue a página
+    e.preventDefault(); // Impede o recarregamento da página
   };
 
-
-//A partir daqui, a tela começa a aparecer para o usuário
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col justify-between">
-      <header className="p-8">
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+      {/* Título */}
+      <header className="w-full max-w-md mt-10 mb-6">
+        <div className="flex items-center justify-start mb-4">
+          {/* Botão de Voltar */}
+          <Link
+            href="#"
+            className="text-gray-800 hover:text-gray-600 transition"
+          >
+            <FaArrowLeft className="text-2xl" />
+          </Link>
+        </div>
+        {/* Logo do app */}
         <Image
           src="../logo.png"
           alt="Logo"
@@ -24,14 +35,20 @@ export default function LoginPage() {
           className="mt-10"
           unoptimized
         />
-        <h1 className="text-3xl font-bold mt-10">Bem vindo</h1>
+        <h1 className="text-3xl font-bold text-gray-800 text-left mt-6">
+          Bem-vindo
+        </h1>
         <p className="text-lg text-gray-600 mt-2">Faça login em sua conta</p>
       </header>
 
-      <div className="p-8 bg-white rounded shadow-md w-full max-w-md mx-auto mb-0">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-2 font-semibold">
+      {/* container centralizado com uma animação */}
+      <div className="w-full max-w-md bg-white p-6 mt-14 rounded-2xl shadow-lg">
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-700"
+            >
               Email
             </label>
             <input
@@ -40,12 +57,15 @@ export default function LoginPage() {
               placeholder="Digite seu email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              className="w-full px-3 py-2 border rounded"
+              className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
               required
             />
           </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block mb-2 font-semibold">
+          <div>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-700"
+            >
               Senha
             </label>
             <input
@@ -54,24 +74,26 @@ export default function LoginPage() {
               placeholder="Digite sua senha"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded"
+              className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
               required
             />
           </div>
           <button
             type="submit"
-            className="w-full bg-teal-500 text-white py-2 rounded hover:bg-blue-600 transition"
+            className="w-full bg-teal-500 text-white p-3 rounded-xl hover:bg-teal-600 transition"
           >
             Entrar
           </button>
         </form>
+
+        {/* links de recuperação de senha e ainda não possuo conta */}
         <div className="mt-4 flex justify-between text-sm">
           <a href="#" className="text-teal-500 hover:underline">
             Esqueci minha senha
           </a>
-          <a href="#" className="text-teal-500 hover:underline">
+          <Link href="/register" className="text-teal-500 hover:underline">
             Não possuo conta
-          </a>
+          </Link>
         </div>
       </div>
     </div>
---
+++ src/app/register/page.tsx
@@ -0,0 +1,264 @@
+"use client";
+
+import { useState, FormEvent, ChangeEvent } from "react";
+import { motion } from "framer-motion";
+
+//importando especificamente um icone de seta
+import { FaArrowLeft } from "react-icons/fa"; 
+
+//definindo os tipos 
+export default function RegisterPage() {
+  const [step, setStep] = useState<number>(1); // Tipo para o estado de etapa
+  const [password, setPassword] = useState<string>(""); // Tipo para a senha
+  const [confirmPassword, setConfirmPassword] = useState<string>(""); // Tipo para a confirmação da senha
+  const [name, setName] = useState<string>(""); // Tipo para o nome
+  const [dob, setDob] = useState<string>(""); // Tipo  para a data de nascimento
+  const [cro, setCro] = useState<string>(""); // Tipo  para o CRO
+  const [rg, setRg] = useState<string>(""); // Tipo para o RG
+  const [email, setEmail] = useState<string>(""); // Tipo  para o email
+  const [phone, setPhone] = useState<string>(""); // Tipo  para o telefone
+  const [error, setError] = useState<string>(""); // Tipo  para a mensagem de erro
+
+  const handleGoBack = () => {
+    // botão de voltar
+    window.history.back();
+  };
+
+  const validatePassword = (password: string): boolean => {
+    // dizendo que a senha precisa de pelo menos 1 letra, 1 número e 1 caractere especial
+    const regex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{6,}$/;
+    return regex.test(password);
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    // Verificação de campos obrigatórios
+    if (
+      !name ||
+      !dob ||
+      !cro ||
+      !email ||
+      !phone ||
+      !password ||
+      !confirmPassword
+    ) {
+      setError("Todos os campos são obrigatórios.");
+      return;
+    }
+
+    // Verificação de senhas
+    if (password !== confirmPassword) {
+      setError("As senhas não coincidem!");
+      return;
+    }
+
+    // Verificação da complexidade da senha
+    if (!validatePassword(password)) {
+      setError(
+        "A senha deve ter pelo menos 6 caracteres, com pelo menos uma letra, um número e um caractere especial."
+      );
+      return;
+    }
+
+    setError(""); // Limpa qualquer erro anterior
+    alert("Cadastro finalizado!");
+  };
+
+  const isStep1Valid = name && dob && cro; // Verificação de campos obrigatórios da etapa 1
+  const isStep2Valid =
+    email && phone && password && confirmPassword && validatePassword(password); // Verificação de campos obrigatórios e senha válida na etapa 2
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement>,
+    setter: React.Dispatch<React.SetStateAction<string>>
+  ) => {
+    setter(e.target.value);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+      {/* Cabeçalho com indicador de progresso */}
+      <header className="w-full max-w-md mt-10 mb-6">
+        <div className="flex items-center justify-start mb-4">
+          {/* Botão de Voltar */}
+          <button
+            onClick={handleGoBack}
+            className="text-gray-800 hover:text-gray-600 transition"
+          >
+            <FaArrowLeft className="text-2xl" />
+          </button>
+        </div>
+        <h1 className="text-3xl font-bold text-gray-800 text-left">Cadastro</h1>
+        <div className="flex justify-center mt-14">
+          <span
+            className={`text-lg font-semibold px-4 py-2 rounded-full ${
+              step === 1
+                ? "bg-teal-500 text-white"
+                : "bg-gray-300 text-gray-600"
+            }`}
+          >
+            1
+          </span>
+          <span className="mx-2 text-gray-500">—</span>
+          <span
+            className={`text-lg font-semibold px-4 py-2 rounded-full ${
+              step === 2
+                ? "bg-teal-500 text-white"
+                : "bg-gray-300 text-gray-600"
+            }`}
+          >
+            2
+          </span>
+        </div>
+        <p className="text-lg text-gray-600 mt-2">
+          {step === 1 ? "Dados pessoais" : "Dados de contato"}
+        </p>
+      </header>
+
+      {/* Container centralizado com animação */}
+      <div className="w-full max-w-md bg-white p-6 rounded-2xl shadow-lg">
+        <motion.div
+          key={step}
+          initial={{ opacity: 0, x: step === 1 ? -50 : 50 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: step === 1 ? 50 : -50 }}
+          transition={{ duration: 0.4 }}
+        >
+          {/* Etapa 1 - Dados Pessoais */}
+          {step === 1 && (
+            <form className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Nome completo
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={(e) => handleChange(e, setName)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite seu nome"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Data de nascimento
+                </label>
+                <input
+                  type="date"
+                  value={dob}
+                  onChange={(e) => handleChange(e, setDob)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  CRO
+                </label>
+                <input
+                  type="number"
+                  value={cro}
+                  onChange={(e) => handleChange(e, setCro)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite seu CRO"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  RG
+                </label>
+                <input
+                  type="number"
+                  value={rg}
+                  onChange={(e) => handleChange(e, setRg)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite seu RG"
+                />
+              </div>
+              <button
+                type="button"
+                onClick={() => setStep(2)}
+                disabled={!isStep1Valid} // Desabilita o botão se os campos obrigatórios não forem preenchidos
+                className="w-full bg-teal-500 text-white p-3 rounded-xl hover:bg-blue-700 transition disabled:opacity-50"
+              >
+                Próximo
+              </button>
+            </form>
+          )}
+
+          {/* Etapa 2 - Dados de Contato */}
+          {step === 2 && (
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  E-mail
+                </label>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => handleChange(e, setEmail)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite seu email"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Telefone
+                </label>
+                <input
+                  type="tel"
+                  value={phone}
+                  onChange={(e) => handleChange(e, setPhone)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite seu telefone"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Senha
+                </label>
+                <input
+                  type="password"
+                  value={password}
+                  onChange={(e) => handleChange(e, setPassword)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Digite sua senha"
+                />
+              </div>
+              <div>
+                <label className="block text-sm font-medium text-gray-700">
+                  Confirme sua senha
+                </label>
+                <input
+                  type="password"
+                  value={confirmPassword}
+                  onChange={(e) => handleChange(e, setConfirmPassword)}
+                  className="w-full p-3 border rounded-xl focus:ring focus:ring-blue-300"
+                  placeholder="Confirme sua senha"
+                />
+              </div>
+              {error && <p className="text-red-500 text-sm">{error}</p>}
+              <div className="flex justify-between">
+                <button
+                  type="button"
+                  onClick={() => setStep(1)}
+                  className="bg-gray-400 text-white p-3 rounded-xl hover:bg-gray-500 transition"
+                >
+                  Voltar
+                </button>
+                <button
+                  type="submit"
+                  disabled={!isStep2Valid} // Desabilita o botão de finalizar se a etapa 2 não for válida
+                  className="bg-teal-500 text-white p-3 rounded-xl hover:bg-teal-700 transition disabled:opacity-50"
+                >
+                  Finalizar Cadastro
+                </button>
+              </div>
+            </form>
+          )}
+        </motion.div>
+      </div>
+    </div>
+  );
+}
