192b19d398a56ec71199f3b3d07428f23d371a1b
Maria Gabriella <mgsc@cesar.school>
2025-04-16 19:32:40 -0300

    alterando nome

---
+++ src/app/types/user.tsx
@@ -0,0 +1,21 @@
+export interface User {
+    _id: string;
+    nome: string;
+    email: string;
+    perfil: 'Admin' | 'Perito' | 'Assistente';
+    rg: string;
+    cro?: string;
+  }
+  
+  export interface AuthResponse {
+    token: string;
+    user: User;
+  }
+  
+  export interface ApiError {
+    response?: {
+      data: {
+        message: string;
+      };
+    };
+  }
