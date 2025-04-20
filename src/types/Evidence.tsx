5fa9e6ab05cb6ed0fdb7e0f7bfdf5427ac1e3391
Maria Gabriella <mgsc@cesar.school>
2025-04-20 14:56:36 -0300

    feat: adicionando link na logo para home

--- src/types/Evidence.tsx
+++ src/types/Evidence.tsx
@@ -1,27 +1,33 @@
+import { User } from "./User"; // Import the User interface
+
 export interface Evidence {
-    _id: string;
-    caso: string; // ID do caso
-    tipo: 'imagem' | 'texto';
-    categoria: string;
-    dataUpload: string;
-    vitima: 'identificada' | 'não identificada';
-    sexo: 'masculino' | 'feminino' | 'indeterminado';
-    estadoCorpo: 'inteiro' | 'fragmentado' | 'carbonizado' | 'putrefacto' | 'esqueleto';
-    lesoes?: string;
-    coletadoPor: string;
-    conteudo?: string; 
-    imagemURL?: string;
-    laudo?: string;
-  }
-  
-  export interface EvidenceResponse {
-    data: Evidence;
-    msg?: string;
-    success?: boolean;
-  }
-  
-  export interface EvidenceListResponse {
-    data: Evidence[];
-    msg?: string;
-    success?: boolean;
-  }
+  _id: string;
+  casoReferencia: string; // Código de Referência do Caso Relacionado
+  tipoEvidencia: 'imagem' | 'texto';
+  categoria: string;
+  dataUpload: string;
+  vitima: 'identificada' | 'não identificada';
+  sexo: 'masculino' | 'feminino' | 'indeterminado';
+  estadoCorpo: 'inteiro' | 'fragmentado' | 'carbonizado' | 'putrefacto' | 'esqueleto';
+  lesoes?: string;
+  coletadoPor: Pick<User, 'nome' | 'email'>; // Populated from User model
+  conteudo?: string;
+  imagemURL?: string;
+  laudo?: string;
+}
+
+export interface EvidenceResponse {
+  msg?: string;
+  evidence?: Evidence;
+}
+
+export interface EvidenceListResponse {
+  msg?: string;
+  evidencias: Evidence[];
+  paginacao: {
+    total: number;
+    paginaAtual: number;
+    porPagina: number;
+    totalPaginas: number;
+  };
+}
