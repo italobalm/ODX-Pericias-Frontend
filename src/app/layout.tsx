42c150f8077bdc524e13f65ec433121ee9d22092
Italo Melo <balmprojects@gmail.com>
2025-04-21 10:29:06 -0300

    feat: adicionando arquivo manifest e icons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,37 @@ export default function RootLayout({
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* favicon original */}
         <link rel="icon" href="/logo.png" />
+
+        {/* ícone PWA Android */}
+        <link
+          rel="icon"
+          href="/AppImages/android/android-launchericon-192-192.png"
+        />
+
+        {/* manifesto para PWA */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* ícone touch para iOS */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/AppImages/ios/180.png"
+        />
+
+        {/* cor da barra de status no Android */}
+        <meta name="theme-color" content="#5ed8b1" />
+
+        {/* habilita modo standalone no iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+
+        {/* estilo da status bar no iOS: black-translucent, black ou default */}
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
       </Head>
       <body className={`${geistSans.variable} antialiased bg-gray-100`}>
         <AuthProvider>
